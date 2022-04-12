115ca5ece88458d13151665c056d388cfba1ec78
nasim <nasim_nozirov@mail.ru>
2022-04-13 01:08:42 +0300

    push №1

--- Lesson-one/ViewController.swift
+++ Lesson-one/ViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private let lightsOff: CGFloat = 0.3
+    private let lightsOn:CGFloat = 1
+    private var currentLight = CurrentLight.red
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 10
+        
+        redView.alpha = lightsOff
+        yellowView.alpha = lightsOff
+        greenView.alpha = lightsOff
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        redView.layer.cornerRadius = redView.frame.width / 2
+        yellowView.layer.cornerRadius = yellowView.frame.width / 2
+        greenView.layer.cornerRadius = greenView.frame.width / 2
+    }
+    
+    @IBAction func toggleColorOnPressed() {
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            greenView.alpha = lightsOff
+            redView.alpha = lightsOn
+            currentLight = .yellow
+        case .yellow:
+            redView.alpha = lightsOff
+            yellowView.alpha = lightsOn
+            currentLight = .green
+        case .green:
+            greenView.alpha = lightsOn
+            yellowView.alpha = lightsOff
+            currentLight = .red
+        }
+    }
 }
 
+extension ViewController {
+    enum CurrentLight {
+        case red, yellow, green
+    }
+}
